7cf0f2ff60ca0417c1791fc954b16a573616ca80
Shubham23jha <skj23072002@gmail.com>
2023-12-11 16:06:27 +0530

    call

---
+++ Class&Object/call.js
@@ -0,0 +1,19 @@
+function SetUsername(username){
+    //complex DB calls
+    this.username = username
+    console.log("called");
+}
+
+function createUser(username, email, password){
+    SetUsername.call(this, username)
+   
+    this.email = email
+    this.password = password
+}
+
+const chai = new createUser("chai", "chai@fb.com", "123")
+console.log(chai);
+/*
+The call method in JavaScript is used to invoke a function with a specified this value and individual arguments (parameters).
+
+*/
